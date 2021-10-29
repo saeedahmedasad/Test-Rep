e754a6c57f29033149bd238386d7bb6d776f4c1f
Saeed <saeedahmed5769644@gmail.com>
2021-10-29 16:39:04 +0500

    Guess Game Update

--- Guess.c
+++ Guess.c
@@ -3,19 +3,37 @@
 int main(void)
 {
     int n;
-    int guess_counter=0;
+    int guess=3;
+    int guess_counter=1;
     int guess_limit=3;
-    int guess=9;
+    int outOfGuess=0;
+
     printf("Guess A Number Between 0 and 20: ");
     scanf("%d", &n);
-    while(n != guess )
+    while(n != guess && outOfGuess ==0 )
         {
+            if(guess_counter<guess_limit)
+            {
             printf("Enter Again: ");
             scanf("%d", &n);
             guess_counter++;
+            }
+            else
+            {
+                outOfGuess = 1;
+            }
+            
             
         
         }
-        printf("You Won The Game!! HURRAY");
+        if(outOfGuess == 1)
+        {
+            printf("You Are Out of Guesses!     Sorry");
+        }
+        else
+        {
+                printf("You Won The Game!! HURRAY");
+        }
+        
 }
    
